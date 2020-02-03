a516b216c9490aff4beaf09035a6a65baddbce5f
Bryan Suryaraso Gani <bryansuryarasogani@gmail.com>
2020-02-04 01:06:07 +0700

    Basic Functions: Navigating, Selecting, Copying, Renaming

--- image_sorting.py
+++ image_sorting.py
@@ -1,8 +1,16 @@
-import os, sys
+import os, sys, shutil
 from tkinter import *
 from PIL import ImageTk, Image
 
 directory_of_images = sys.argv[1]
+
+filenames_list = []
+for filename in os.listdir(directory_of_images):
+    if (not filename.endswith("_n.JPG")) and (not filename.endswith("_p.JPG")) and filename.endswith(".JPG"):
+        filenames_list.append(filename)
+
+number_of_files = len(filenames_list)
+current_files = 0
 
 folder_of_positive = "Positive"
 folder_of_negative = "Negative"
@@ -16,35 +24,94 @@ if not os.path.exists(directory_of_positive):
 if not os.path.exists(directory_of_negative):
     os.mkdir(directory_of_negative)
 
+#Root
+root = Tk()
+root.title("Image Sorting")
 
+#Image Viwer
+image = 0
+image_view = 0
 
-def main():
-    #Root
-    root = Tk()
-    root.title("Image Sorting")
-
-    #Buttons
-    positive_button = Button(root, text = "Positive\n(Cracks)"
-    , pady = 5, width = 15, bg = "green", command = positive())
-    negative_button = Button(root, text = "Negative\n(No Cracks)"
-    , pady = 5, width = 15, bg = "red", command = negative())
-
-    #Image Viwer
-    image = ImageTk.PhotoImage(Image.open("Result_227\IMG_0056_00.JPG"))
+def change_image(index):
+    global image
+    global image_view
+    image = ImageTk.PhotoImage(Image.open(os.path.join(directory_of_images
+    , filenames_list[index])))
     image_view = Label(image = image)
-
-    #Positioning
     image_view.grid(row = 0, column = 0, columnspan = 2)
-    positive_button.grid(row = 1, column = 0)
-    negative_button.grid(row = 1, column = 1)
 
-    root.mainloop()
+change_image(current_files)
+
+def insert_indicator(indicator):
+    filename = filenames_list[current_files]
+    index = filename.find('.')
+    return filename[:index] + indicator + filename[index:]
+
+def copy_to(directory_of_transfer, indicator):
+    shutil.copy2(os.path.join(directory_of_images, filenames_list[current_files]),
+    directory_of_transfer)
+    os.rename(os.path.join(directory_of_images, filenames_list[current_files])
+    , os.path.join(directory_of_images, insert_indicator(indicator)))
 
 def positive():
-    return
+    global image_view
+    global current_files
+    global number_of_files
+    image_view.grid_forget()
+    copy_to(directory_of_positive, "_p")
+    filenames_list.pop(current_files)
+    number_of_files -= 1
+    if number_of_files == current_files:
+        current_files = 0
+    change_image(current_files)
 
 def negative():
+    global image_view
+    global current_files
+    global number_of_files
+    image_view.grid_forget()
+    copy_to(directory_of_negative, "_n")
+    filenames_list.pop(current_files)
+    number_of_files -= 1
+    if number_of_files == current_files:
+        current_files = 0
+    change_image(current_files)
+
+def next():
+    global image_view
+    global current_files
+    image_view.grid_forget()
+    current_files =  (current_files + 1) % number_of_files
+    change_image(current_files)
     return
 
-if __name__ == "__main__":
-    main()
+def previous():
+    global image_view
+    global current_files
+    image_view.grid_forget()
+    if current_files == 0:
+        current_files = number_of_files - 1
+    else:
+        current_files -= 1
+        current_files %= number_of_files
+    change_image(current_files)
+    return
+
+#Buttons
+positive_button = Button(root, text = "Positive\n(Cracks)"
+, pady = 5, width = 15, bg = "green", command = positive)
+negative_button = Button(root, text = "Negative\n(No Cracks)"
+, pady = 5, width = 15, bg = "red", command = negative)
+
+next_button = Button(root, text = "Next >"
+, pady = 5, width = 15, command = next)
+previous_button = Button(root, text = "Previous <"
+, pady = 5, width = 15, command = previous)
+
+#Positioning
+positive_button.grid(row = 1, column = 0)
+negative_button.grid(row = 1, column = 1)
+next_button.grid(row = 2, column = 1)
+previous_button.grid(row = 2, column = 0)
+
+root.mainloop()
