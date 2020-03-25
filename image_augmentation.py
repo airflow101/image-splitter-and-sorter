0ed97cba624f6ca97e37e1d0239a7145bfda64ae
Bryan Suryaraso Gani <bryansuryarasogani@gmail.com>
2020-03-26 00:23:46 +0800

    Multicore Parameter Adjustment

--- image_augmentation.py
+++ image_augmentation.py
@@ -1,5 +1,6 @@
 import os, sys
 import imageio
+import time
 from imgaug import augmenters as iaa
 from imgaug.augmentables.batches import UnnormalizedBatch
 
@@ -26,7 +27,7 @@ for folder_name in folder_names:
 #Rotation and Translation
 def transrot(batches):
     aff = iaa.Sequential([iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}), iaa.Rot90((1,3))])
-    with aff.pool(processes=3, maxtasksperchild=10, seed=1) as pool:
+    with aff.pool(processes=3, maxtasksperchild=12, seed=1) as pool:
         batches_aug = pool.map_batches(batches)
     for i in range(len(filenames_result)):
         imageio.imwrite(os.path.join(folder_dir[0], filenames_result[i]), batches_aug[i//BATCH_SIZE].images_aug[i%BATCH_SIZE])
@@ -34,7 +35,7 @@ def transrot(batches):
 
 #Gaussian Blur
 def gaussian(batches):
-    with iaa.GaussianBlur(sigma=(0.0, 3.0)).pool(processes=3, maxtasksperchild=10, seed=1) as pool:
+    with iaa.GaussianBlur(sigma=(0.0, 3.0)).pool(processes=3, maxtasksperchild=12, seed=1) as pool:
         batches_aug = pool.map_batches(batches)
     for i in range(len(filenames_result)):
         imageio.imwrite(os.path.join(folder_dir[1], filenames_result[i]), batches_aug[i//BATCH_SIZE].images_aug[i%BATCH_SIZE])
@@ -42,14 +43,14 @@ def gaussian(batches):
 #Brightness and Contrast
 def brightcont(batches):
     augm = iaa.Sequential([iaa.MultiplyBrightness((0.5, 1.5)), iaa.GammaContrast((0.5, 2.0))])
-    with augm.pool(processes=3, maxtasksperchild=10, seed=1) as pool:
+    with augm.pool(processes=3, maxtasksperchild=12, seed=1) as pool:
         batches_aug = pool.map_batches(batches)
     for i in range(len(filenames_result)):
         imageio.imwrite(os.path.join(folder_dir[2], filenames_result[i]), batches_aug[i//BATCH_SIZE].images_aug[i%BATCH_SIZE])
 
 #Salt and Pepper
 def saltpepper(batches):
-    with iaa.SaltAndPepper(0.1).pool(processes=3, maxtasksperchild=10, seed=1) as pool:
+    with iaa.SaltAndPepper(0.1).pool(processes=3, maxtasksperchild=12, seed=1) as pool:
         batches_aug = pool.map_batches(batches)
     for i in range(len(filenames_result)):
         imageio.imwrite(os.path.join(folder_dir[3], filenames_result[i]), batches_aug[i//BATCH_SIZE].images_aug[i%BATCH_SIZE])
@@ -57,10 +58,13 @@ def saltpepper(batches):
 
 #Augmenter Initialization
 def augment(batches):
+    time_start = time.time()
     transrot(batches)
     gaussian(batches)
     brightcont(batches)
     saltpepper(batches)
+    time_end = time.time()
+    print("%.2fs" % (time_end-time_start))
 
 
 #Read directory_of_images
